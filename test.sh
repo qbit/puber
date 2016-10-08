6884e55888d87bc8f6e0215e3fa1cbdeb7bda372
Aaron Bieber <aaron@bolddaemon.com>
2016-10-08 09:29:20 -0600

    expand test to handle more cases

--- test.sh
+++ test.sh
@@ -10,14 +10,23 @@ getKey
 RES=$(curl -sH "Content-Type: application/json" -X POST -d "{\"user\": \"${KEY}\", \"pubKey\": \"asdfasdfasdfasdf\", \"yKey\": \"${YK}\"}" http://localhost:8081/add)
 
 if [ "$RES" == "Added" ]; then
-    echo "[success]: added key '${KEY}'"
-    #	getKey
-    #	RES=$(curl -sH "Content-Type: application/json" -X POST -d "{\"user\": \"${KEY}\", \"yKey\": \"${YK}\"}" http://localhost:8081/rm)
-    #	if [ "$RES" == "Removed" ]; then
-    #		echo "[success]: removing key '${KEY}'"
-    #	else
-    #		echo "[fail]: removing key '${KEY}' (${RES})"
-    #	fi
+	echo "[success]: added key '${KEY}'"
 else
-    echo "[fail]: adding key '${KEY}' (${RES})"
+	echo "[fail]: adding key '${KEY}' (${RES})"
+fi
+
+getKey
+RES=$(curl -sH "Content-Type: application/json" -X POST -d "{\"user\": \"debug\", \"yKey\": \"${YK}\", \"pubkey\": \"debug key two\"}" http://localhost:8081/rm)
+if [ "$RES" == "Removed 'debug key two' from 'debug'" ]; then
+	echo "[success]: removing key 'debug key two' for user 'debug'"
+else
+	echo "[fail]: removing key '${KEY}' (${RES})"
+fi
+
+getKey
+RES=$(curl -sH "Content-Type: application/json" -X POST -d "{\"user\": \"debug\", \"yKey\": \"${YK}\"}" http://localhost:8081/rm)
+if [ "$RES" == "Removed" ]; then
+	echo "[success]: removing all keys 'debug'"
+else
+	echo "[fail]: removing key '${KEY}' (${RES})"
 fi
