f4111c800f4d09a026a705f3ad419f94bb34b000
Aaron Bieber <aaron@bolddaemon.com>
2016-10-08 12:56:47 -0600

    finish redis backend

--- backend/redis_backend.go
+++ backend/redis_backend.go
@@ -25,15 +25,82 @@ func (r *RedisStore) Close() {
 
 // Add inserts a key into the redis backend
 func (r *RedisStore) Add(user string, key string) (bool, error) {
-	var resp = true
 	reply := r.client.Cmd("rpush", user, key)
 	if reply.Err != nil {
 		return false, reply.Err
 	}
-	return resp, nil
+	return true, nil
 }
 
 // RM removes a key from the lset
 func (r *RedisStore) RM(user string, key string) (bool, error) {
+	reply := r.client.Cmd("lrem", user, -1, key)
+
+	if reply.Err != nil {
+		return false, reply.Err
+	}
 	return true, nil
+}
+
+// RMAll removes all the keys for a given user
+func (r *RedisStore) RMAll(user string) (bool, error) {
+	reply := r.client.Cmd("del", user)
+	if reply.Err != nil {
+		return false, reply.Err
+	}
+
+	return true, nil
+}
+
+// Get returns all keys for user
+func (r *RedisStore) Get(user string) ([]string, error) {
+	keys, err := r.client.Cmd("lrange", user, 0, 1).List()
+	if err != nil {
+		return nil, err
+	}
+
+	return keys, nil
+}
+
+// GetAll returns all keys for all users
+func (r *RedisStore) GetAll() ([]string, error) {
+	var s []string
+	reply := r.client.Cmd("keys", "*")
+	keys, err := reply.List()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, k := range keys {
+		uKeys, err := r.client.Cmd("lrange", k, 0, -1).List()
+		if err != nil {
+			return nil, err
+		}
+		for i := range uKeys {
+			s = append(s, uKeys[i])
+		}
+	}
+
+	return s, nil
+}
+
+// GetCount gets a count of all the keys
+func (r *RedisStore) GetCount() (int, error) {
+	s := 0
+	keys, err := r.client.Cmd("keys", "*").List()
+	if err != nil {
+		return -1, err
+	}
+
+	for _, k := range keys {
+		uKeys, err := r.client.Cmd("lrange", k, 0, -1).List()
+		if err != nil {
+			return -1, err
+		}
+		for _ = range uKeys {
+			s++
+		}
+	}
+
+	return s, nil
 }
