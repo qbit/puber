4792307c70f2a795f2bb1b1a3b4ba7d3cd96889e
Aaron Bieber <aaron@bolddaemon.com>
2016-10-08 10:03:27 -0600

    loosen GetAll

--- backend/backend.go
+++ backend/backend.go
@@ -1,12 +1,18 @@
 package backend
 
+// Options stores our various ways to connect to backends.
+// Redis for example.
+type Options struct {
+}
+
 // Backend defines our various methods for connecting to data stores.
 type Backend interface {
 	Add(string, string) (bool, error)
 	RM(string, string) (bool, error)
 	RMAll(string) (bool, error)
 	Get(string) ([]string, error)
-	GetAll() (map[string][]string, error)
+	GetAll() ([]string, error)
 	GetCount() (int, error)
-	Init()
+	Init() error
+	Close()
 }
